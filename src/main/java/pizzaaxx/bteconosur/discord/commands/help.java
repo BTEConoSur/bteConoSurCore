817ceffa4e48530d94f10a64025af8b661619629
PIZZAAXX <pedro2massmann@gmail.com>
2022-02-10 16:15:30 +0100

    Made root `/help` command work. Added deletion after time to avoid channel textwalls.

--- src/main/java/pizzaaxx/bteconosur/discord/commands/help.java
+++ src/main/java/pizzaaxx/bteconosur/discord/commands/help.java
@@ -1,27 +1,28 @@
 package pizzaaxx.bteconosur.discord.commands;
 
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.MessageBuilder;
 import net.dv8tion.jda.api.entities.Emoji;
-import net.dv8tion.jda.api.entities.Emote;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.GenericEvent;
-import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
 import net.dv8tion.jda.api.interactions.components.ActionRow;
 import net.dv8tion.jda.api.interactions.components.Button;
 import net.dv8tion.jda.api.interactions.components.ButtonStyle;
-import org.apache.commons.lang.StringUtils;
 import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
 import pizzaaxx.bteconosur.yaml.YamlManager;
 
 import java.awt.*;
-import java.util.*;
-import java.util.List;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static pizzaaxx.bteconosur.bteConoSur.pluginFolder;
+import static pizzaaxx.bteconosur.discord.bot.conoSurBot;
 
 public class help implements EventListener {
     @Override
@@ -36,14 +37,40 @@ public class help implements EventListener {
                         if (args.length == 1) {
                             EmbedBuilder choose = new EmbedBuilder();
                             choose.setColor(new Color(0,255,42));
-                            choose.setTitle("Elige que comandos quieres ver.");
+                            choose.setTitle("Elige qué comandos quieres ver.");
                             ActionRow actionRow = ActionRow.of(
                                     Button.of(ButtonStyle.SECONDARY, "minecraft", "Minecraft", Emoji.fromMarkdown("<:abcdefg:941030570048258048>")),
                                     Button.of(ButtonStyle.SECONDARY, "discord", "Discord", Emoji.fromMarkdown("<:qwertyu:853810678774759434>"))
                             );
+
                             e.getTextChannel().sendMessageEmbeds(choose.build()).setActionRows(actionRow).reference(e.getMessage()).mentionRepliedUser(false).queue();
                         } else {
 
+
+                            YamlManager yaml = new YamlManager(pluginFolder, "help.yml");
+                            String platform = null;
+                            if (((Map<String, Object>) yaml.getValue("minecraft")).containsKey(args[1])) {
+                                platform = "Discord";
+                            } else if (((Map<String, Object>) yaml.getValue("discord")).containsKey(args[1])) {
+                                platform = "Minecraft";
+                            }
+                            String path = platform + "." + String.join(".subcommands.", Arrays.asList(args).subList(0, args.length));
+                            Map<String, Object> command = (Map<String, Object>) yaml.getValue(path);
+
+                            if (platform != null && command != null) {
+                                EmbedBuilder help = new EmbedBuilder();
+                                help.setColor(new Color(0, 255, 42));
+                                help.setTitle((String) command.get("usage"));
+
+
+
+                            } else {
+                                EmbedBuilder error = new EmbedBuilder();
+                                error.setColor(new Color(255, 0,0));
+                                error.setTitle("El comando/subcomando introducido no existe.");
+                                e.getTextChannel().sendMessageEmbeds(error.build()).reference(e.getMessage()).mentionRepliedUser(false).queue(msg -> msg.delete().queueAfter(1, TimeUnit.MINUTES));
+                                e.getMessage().delete().queueAfter(1, TimeUnit.MINUTES);
+                            }
                         }
                     }
                 }
