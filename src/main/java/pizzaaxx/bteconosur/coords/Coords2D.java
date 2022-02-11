4796c81768bc0f76f5de0e7f1f6c8b67ea72e5d8
SrBenja007 <supremobenjamin.cia@gmail.com>
2022-02-10 22:59:27 -0300

    code improvements in Coords2D

--- src/main/java/pizzaaxx/bteconosur/coords/Coords2D.java
+++ src/main/java/pizzaaxx/bteconosur/coords/Coords2D.java
@@ -1,7 +1,6 @@
 package pizzaaxx.bteconosur.coords;
 
 import com.sk89q.worldedit.BlockVector2D;
-import jdk.nashorn.internal.ir.Block;
 import net.buildtheearth.terraplusplus.generator.EarthGeneratorSettings;
 import net.buildtheearth.terraplusplus.projection.GeographicProjection;
 import net.buildtheearth.terraplusplus.projection.OutOfProjectionBoundsException;
@@ -11,15 +10,14 @@ import static pizzaaxx.bteconosur.BteConoSur.mainWorld;
 
 public class Coords2D {
 
-    public static GeographicProjection geographicProjection = EarthGeneratorSettings.parse(EarthGeneratorSettings.BTE_DEFAULT_SETTINGS).projection();
-
     private double x;
     private double z;
     private double lon;
     private double lat;
 
-    // CONSTRUCTOR
     public Coords2D(Double lat, Double lon) {
+        GeographicProjection geographicProjection = EarthGeneratorSettings.parse(EarthGeneratorSettings.BTE_DEFAULT_SETTINGS).projection();
+
         try {
             double[] mcCoords = geographicProjection.fromGeo(lon, lat);
             this.lon = lat;
@@ -32,30 +30,12 @@ public class Coords2D {
     }
 
     public Coords2D(BlockVector2D point) {
-        try {
-            double[] geoCoords = geographicProjection.toGeo(point.getX(), point.getZ());
-            this.x = point.getX();
-            this.z = point.getZ();
-            this.lon = geoCoords[0];
-            this.lat = geoCoords[1];
-        } catch (OutOfProjectionBoundsException e) {
-            e.printStackTrace();
-        }
+        this(point.getX(), point.getZ());
     }
 
     public Coords2D(Location location) {
-        try {
-            double[] geoCoords = geographicProjection.toGeo(location.getX(), location.getZ());
-            this.x = location.getX();
-            this.z = location.getZ();
-            this.lon = geoCoords[0];
-            this.lat = geoCoords[1];
-        } catch (OutOfProjectionBoundsException e) {
-            e.printStackTrace();
-        }
+        this(location.getX(), location.getZ());
     }
-
-    // GETTER
 
     public BlockVector2D toBlockVector2D() {
         return new BlockVector2D(this.x, this.z);
@@ -81,7 +61,7 @@ public class Coords2D {
         return z;
     }
 
-    public Integer getHighestY() {
+    public int getHighestY() {
         return mainWorld.getHighestBlockYAt(new Location(mainWorld, this.x, 100, this.z));
     }
 }
