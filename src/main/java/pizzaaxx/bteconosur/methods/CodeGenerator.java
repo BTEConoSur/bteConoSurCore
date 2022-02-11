a9b47f4f5af1fb6442203fcd0e291e9635c09a90
SrBenja007 <supremobenjamin.cia@gmail.com>
2022-02-10 23:20:11 -0300

    improved and optimized code generation class

--- src/main/java/pizzaaxx/bteconosur/methods/CodeGenerator.java
+++ src/main/java/pizzaaxx/bteconosur/methods/CodeGenerator.java
@@ -5,14 +5,17 @@ import java.util.*;
 public class CodeGenerator {
 
     private static final Random RANDOM = new Random();
+    private static final String[] CHARACTERS = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
 
     public static String generateCode(Integer length) {
-        List<String> chars = new ArrayList<>(Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"));
-        String code = "";
-        for (int i=1; i<=length; i++) {
-            int rndmNumber = rndm.nextInt(chars.size());
-            code = code + chars.get(rndmNumber);
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 1; i <= length; i++) {
+            int number = RANDOM.nextInt(CHARACTERS.length);
+            stringBuilder.append(CHARACTERS[number]);
         }
-        return code;
+
+        return stringBuilder.toString();
     }
+
 }
