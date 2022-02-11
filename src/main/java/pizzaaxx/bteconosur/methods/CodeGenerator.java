1bd4c01e1b121961e1ef1f29884b4d36c55d3e81
SrBenja007 <supremobenjamin.cia@gmail.com>
2022-02-10 23:16:52 -0300

    fixed an optimization problem

--- src/main/java/pizzaaxx/bteconosur/methods/CodeGenerator.java
+++ src/main/java/pizzaaxx/bteconosur/methods/CodeGenerator.java
@@ -4,9 +4,10 @@ import java.util.*;
 
 public class CodeGenerator {
 
+    private static final Random RANDOM = new Random();
+
     public static String generateCode(Integer length) {
         List<String> chars = new ArrayList<>(Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"));
-        Random rndm = new Random();
         String code = "";
         for (int i=1; i<=length; i++) {
             int rndmNumber = rndm.nextInt(chars.size());
