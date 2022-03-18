de19dd3dc31e18dc60d634f12edb785cff3444be
pedro <pedro2massmann@gmail.com>
2022-03-17 21:53:18 -0300

    (UNUSABLE) (UNTESTED) Fixed `NicknameCommand.java`.

--- src/main/java/pizzaaxx/bteconosur/commands/NickNameCommand.java
+++ src/main/java/pizzaaxx/bteconosur/commands/NickNameCommand.java
@@ -27,11 +27,11 @@ public class NickNameCommand implements CommandExecutor {
 
                         if (args.length > 1) {
                             if (args[1].matches("[a-zA-Z0-9_]{1,16}")) {
-                                if (!(new ServerPlayer(target).getName().equalsIgnoreCase(args[1]))) {
-                                    t.setNickname(args[1]);
+                                if (!t.getName().equalsIgnoreCase(args[1])) {
+                                    t.getChatManager().setNick(args[1]);
                                     p.sendMessage(nickPrefix + "Has establecido el apodo de §a" + new ServerPlayer(target).getName() + "§f en §a" + args[1] + "§f.");
                                 } else {
-                                    t.setNickname(null);
+                                    t.getChatManager().setNick(null);
                                     p.sendMessage(nickPrefix + "Has reeestablecido el apodo de §a" + new ServerPlayer(target).getName() + "§f.");
                                 }
                             } else {
@@ -46,10 +46,10 @@ public class NickNameCommand implements CommandExecutor {
                 } else {
                     if ((!p.hasPermission("bteconsur.nickname.others") && args[0].matches("[a-zA-Z0-9_]{1,16}")) || (p.hasPermission("bteconsur.nickname.others") && args[0].matches("[a-zA-Z0-9_&]{1,16}"))) {
                         if (!(new ServerPlayer(p).getName().equalsIgnoreCase(args[0]))) {
-                            s.setNickname(args[0]);
+                            s.getChatManager().setNick(args[0]);
                             p.sendMessage(nickPrefix + "Has establecido tu apodo en §a" + args[0].replace("&", "§") + "§f.");
                         } else {
-                            s.setNickname(null);
+                            s.getChatManager().setNick(null);
                             p.sendMessage(nickPrefix + "Has reeestablecido tu apodo.");
                         }
                     } else {
