8aec6821c83cf4c58c2c21f2261e5b11e3f6b1cc
Benjamín Miranda <supremobenjamin.cia@gmail.com>
2022-04-01 23:19:06 -0300

    adapt the gradle

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -17,4 +17,13 @@ repositories {
 
 dependencies {
     compileOnly("com.destroystokyo.paper:paper-api:1.12.2-R0.1-SNAPSHOT")
+    compileOnly("net.luckperms:api:1.12.2-R0.1-SNAPSHOT")
+    compileOnly("com.sk89q.worldedit:worldedit-core:6.1.1-SNAPSHOT")
+    compileOnly("com.sk89q.worldedit:worldedit-bukkit:6.1.1-SNAPSHOT")
+    compileOnly("com.sk89q.worldguard:worldguard-legacy:6.2")
+    implementation("net.dv8tion:JDA:JDA:5.0.0-alpha.4")
+    implementation("com.github.BuildTheEarth:terraplusplus:terraplusplus:v1.1.0")
+    implementation("xyz.upperlevel.spigot.book:spigot-book-api:v1.1.0")
+    implementation("fr.minuskube:netherboard-bukki:1.2.2")
 }
+
