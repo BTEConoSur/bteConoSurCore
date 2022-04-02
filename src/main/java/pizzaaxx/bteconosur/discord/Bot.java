d6b6cbd8c1b10a222343ced28f77042b87a2a494
Benjamín Miranda <supremobenjamin.cia@gmail.com>
2022-04-02 10:06:41 -0300

    delete uneccesary src project folder

--- src/main/java/pizzaaxx/bteconosur/discord/Bot.java
+++
@@ -1,9 +0,0 @@
-package pizzaaxx.bteconosur.discord;
-
-import net.dv8tion.jda.api.JDA;
-
-public class Bot {
-
-    public static JDA conoSurBot;
-
-}
--- src/main/java/pizzaaxx/bteconosur/novedades/OnJoin.java
+++
@@ -1,4 +0,0 @@
-package pizzaaxx.bteconosur.novedades;
-
-public class OnJoin {
-}
