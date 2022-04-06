bfd09942e52a6e16ff9e353b1d0adc6b1a806473
Benjamín Miranda <supremobenjamin.cia@gmail.com>
2022-04-05 20:32:47 -0400

    u

--- bukkit/src/main/java/pizzaaxx/bteconosur/storage/sql/SqlObjectAdapter.java
+++ bukkit/src/main/java/pizzaaxx/bteconosur/storage/sql/SqlObjectAdapter.java
@@ -1,6 +1,8 @@
 package pizzaaxx.bteconosur.storage.sql;
 
+import pizzaaxx.bteconosur.storage.ObjectAdapter;
+
 import java.sql.ResultSet;
 
-public interface SqlObjectAdapter<T> extends ObjectAdapter<T, ResultSet, > {
+public interface SqlObjectAdapter<T> extends ObjectAdapter<T, ResultSet, QueryStatement> {
 }
