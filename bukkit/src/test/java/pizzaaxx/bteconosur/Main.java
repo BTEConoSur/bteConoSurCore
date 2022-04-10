1a5d3c80ec51f747d6d5fe68f99e1403b48ed748
Benjamín Miranda <supremobenjamin.cia@gmail.com>
2022-04-09 23:58:30 -0400

    test

--- bukkit/src/test/java/pizzaaxx/bteconosur/Main.java
+++ bukkit/src/test/java/pizzaaxx/bteconosur/Main.java
@@ -1,9 +1,22 @@
 package pizzaaxx.bteconosur;
 
+import pizzaaxx.bteconosur.storage.DataSource;
+import pizzaaxx.bteconosur.storage.connection.DatabaseConnection;
+import pizzaaxx.bteconosur.storage.sql.MysqlConnection;
+
+import java.sql.Connection;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
-    public static void main(String[] args){
-        System.out.println("test");
+    public static void main(String[] args) throws ClassNotFoundException {
+
+        Map<String,String> source = new HashMap<>();
+        DataSource dataSource = new DataSource();
+
+        DatabaseConnection<Connection>
+                databaseConnection = new MysqlConnection()
     }
 
 }
