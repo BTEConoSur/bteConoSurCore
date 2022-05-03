e5c46c70474f7e3ddafd26e27610b2bc27b7b5ee
Benjamín Miranda <supremobenjamin.cia@gmail.com>
2022-05-03 15:51:36 -0400

    finish first part user

--- bukkit/src/main/java/pizzaaxx/bteconosur/server/player/NewServerPlayer.java
+++ bukkit/src/main/java/pizzaaxx/bteconosur/server/player/NewServerPlayer.java
@@ -6,6 +6,26 @@ public class NewServerPlayer {
 
     private final UUID identifier;
     private final String discriminator;
+    private final int points;
 
+    public NewServerPlayer(UUID identifier,
+                           String discriminator,
+                           int points) {
+        this.identifier = identifier;
+        this.discriminator = discriminator;
+        this.points = points;
+    }
+
+    public UUID getIdentifier() {
+        return identifier;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public String getDiscriminator() {
+        return discriminator;
+    }
 
 }
