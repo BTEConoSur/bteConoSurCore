425f643757da9964edde0024705a5311845ed54d
Benjamín Miranda <supremobenjamin.cia@gmail.com>
2022-05-08 16:27:50 -0400

    finish player chat

--- bukkit/src/main/java/pizzaaxx/bteconosur/listener/AsyncPlayerChatListener.java
+++ bukkit/src/main/java/pizzaaxx/bteconosur/listener/AsyncPlayerChatListener.java
@@ -1,20 +1,53 @@
 package pizzaaxx.bteconosur.listener;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import pizzaaxx.bteconosur.server.player.NewServerPlayer;
 import pizzaaxx.bteconosur.server.player.PlayerRegistry;
+import pizzaaxx.bteconosur.yaml.Configuration;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class AsyncPlayerChatListener implements Listener {
 
     private final PlayerRegistry playerRegistry;
+    private final Configuration chatsConfiguration;
 
-    public AsyncPlayerChatListener(PlayerRegistry playerRegistry) {
+    public AsyncPlayerChatListener(PlayerRegistry playerRegistry,
+                                   Configuration chatsConfiguration) {
         this.playerRegistry = playerRegistry;
+        this.chatsConfiguration = chatsConfiguration;
     }
 
     @EventHandler
     public void onChat(AsyncPlayerChatEvent event) {
+
+        Player player = event.getPlayer();
+        String message = event.getMessage();
+
+        NewServerPlayer newServerPlayer =
+                playerRegistry.get(player.getUniqueId());
+
+        String channelChat = newServerPlayer.getChannelChat();
+        event.setCancelled(true);
+
+        String format = chatsConfiguration.getString(channelChat + ".format");
+        String permission = chatsConfiguration.getStringWithoutColors(channelChat + ".permission");
+
+        format = format.replace("%player%", player.getName())
+                .replace("%message%", message);
+
+        for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+            if (permission != null && onlinePlayer.hasPermission(permission)) {
+                onlinePlayer.sendMessage(
+                        format
+                );
+            }
+        }
 
     }
 
