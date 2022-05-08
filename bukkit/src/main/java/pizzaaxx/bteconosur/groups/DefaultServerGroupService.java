85ffb7521134fcefbbf2dc9048f6f5dcb41f0e6b
Benjamín Miranda <supremobenjamin.cia@gmail.com>
2022-05-07 23:39:44 -0400

    added get

---
+++ bukkit/src/main/java/pizzaaxx/bteconosur/groups/DefaultServerGroupService.java
@@ -0,0 +1,55 @@
+package pizzaaxx.bteconosur.groups;
+
+import org.bukkit.command.CommandSender;
+import pizzaaxx.bteconosur.server.player.NewServerPlayer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DefaultServerGroupService implements ServerGroupService {
+
+    private final Map<Integer, ServerGroup> primaryGroups = new HashMap<>();
+    private final Map<Integer, ServerGroup> secondsGroups = new HashMap<>();
+
+    @Override
+    public void updatePrimaryGroup(CommandSender sender, NewServerPlayer newServerPlayer, ServerGroup serverGroup) {
+
+    }
+
+    @Override
+    public void addSecondaryGroup(CommandSender sender, NewServerPlayer newServerPlayer, ServerGroup serverGroup) {
+
+    }
+
+    @Override
+    public void deleteSecondaryGroup(CommandSender sender, NewServerPlayer newServerPlayer, ServerGroup serverGroup) {
+
+    }
+
+    @Override
+    public ServerGroup getGroup(int identifier) {
+        ServerGroup serverGroup = getPrimaryGroup(identifier);
+
+        if (serverGroup != null) {
+            return serverGroup;
+        }
+
+        return getSecondaryGroup(identifier);
+    }
+
+    @Override
+    public ServerGroup getPrimaryGroup(int identifier) {
+        return primaryGroups.get(identifier);
+    }
+
+    @Override
+    public ServerGroup getSecondaryGroup(int identifier) {
+        return secondsGroups.get(identifier);
+    }
+
+    @Override
+    public void registerGroup(ServerGroup serverGroup) {
+
+    }
+
+}
