36d61d14a249d80c9c9773be836db3de0b2a9380
Benjamín Miranda <supremobenjamin.cia@gmail.com>
2022-05-08 16:38:13 -0400

    added arguments class

---
+++ bukkit/src/main/java/pizzaaxx/bteconosur/commands/ChatCommand.java
@@ -0,0 +1,13 @@
+package pizzaaxx.bteconosur.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class ChatCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
+}
---
+++ bukkit/src/main/java/pizzaaxx/bteconosur/helper/Arguments.java
@@ -0,0 +1,54 @@
+package pizzaaxx.bteconosur.helper;
+
+public class Arguments {
+
+    private final String[] arguments;
+    private static final String SPACE = " ";
+    private static final String EMPTY = "";
+
+    public Arguments(String[] arguments) {
+        this.arguments = arguments;
+    }
+
+    public String join() {
+        return String.join(SPACE, arguments);
+    }
+
+    public String getArgument(int size) {
+        if (size > arguments.length) {
+            return EMPTY;
+        }
+        return arguments[size];
+    }
+
+    public String contactArguments(int first) {
+        return contactArguments(first, arguments.length);
+    }
+
+    public String contactArguments(int first, int last) {
+        if (first > arguments.length || last > arguments.length) {
+            return EMPTY;
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < last; i++) {
+            stringBuilder.append(arguments[i])
+                    .append(SPACE);
+        }
+
+        return stringBuilder.toString();
+    }
+
+    public void deleteArgument(int index) {
+        if (index > arguments.length) {
+            return;
+        }
+
+        arguments[index] = EMPTY;
+    }
+
+    public static Arguments from(String[] arguments) {
+        return new Arguments(arguments);
+    }
+
+}
