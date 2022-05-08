a4baf3178e92abe8edc23f55b03dcace0ffd4119
Benjamín Miranda <supremobenjamin.cia@gmail.com>
2022-05-07 23:12:08 -0400

    assigned primary group

---
+++ bukkit/src/main/java/pizzaaxx/bteconosur/groups/ServerGroup.java
@@ -0,0 +1,38 @@
+package pizzaaxx.bteconosur.groups;
+
+import pizzaaxx.bteconosur.server.player.Identifiable;
+
+import java.awt.*;
+
+public class ServerGroup implements Identifiable {
+
+    private final String name;
+    private final int identifier;
+    private final Color color;
+    private final boolean isPrimary;
+
+    public ServerGroup(String name, int identifier, Color color, boolean isPrimary) {
+        this.name = name;
+        this.identifier = identifier;
+        this.color = color;
+        this.isPrimary = isPrimary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getId() {
+        return Integer.toString(identifier);
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public boolean isPrimary() {
+        return isPrimary;
+    }
+
+}
