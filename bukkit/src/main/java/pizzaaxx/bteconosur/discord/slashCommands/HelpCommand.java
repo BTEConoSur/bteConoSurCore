63c1afd1ec02404affea416ffe5c015bdc659eea
PIZZAAXX <pedro2massmann@gmail.com>
2022-06-01 23:03:10 -0400

    worked on help command, still have to do button listener

--- bukkit/src/main/java/pizzaaxx/bteconosur/discord/slashCommands/HelpCommand.java
+++ bukkit/src/main/java/pizzaaxx/bteconosur/discord/slashCommands/HelpCommand.java
@@ -1,9 +1,15 @@
 package pizzaaxx.bteconosur.discord.slashCommands;
 
+import com.google.common.collect.Lists;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Emoji;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.components.ActionRow;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import net.dv8tion.jda.api.interactions.components.buttons.ButtonStyle;
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.configuration.ConfigurationSection;
 import pizzaaxx.bteconosur.yaml.Configuration;
 
@@ -36,15 +42,81 @@ public class HelpCommand extends ListenerAdapter {
 
                 for (String key : rootSection.getKeys(false)) {
 
-                    List
+                    List<String> lines = characters.getOrDefault(key.substring(0, 1), new ArrayList<>());
+
+                    lines.add("• `" +  key + "`: " + rootSection.getString(key + ".description"));
+
+                    characters.put(key.substring(0, 1), lines);
 
                 }
+
+                List<String> keys = new ArrayList<>(characters.keySet());
+                Collections.sort(keys);
+                List<List<String>> dividedCharacters = Lists.partition(keys, 6);
+
+                EmbedBuilder builder = new EmbedBuilder();
+                builder.setColor(Color.GREEN);
+                builder.setTitle("Ayuda de comandos de " + StringUtils.capitalize(root));
+                builder.addField("Sistema de ayuda:", "Usa `/help [comando]` para obtener más información de cada comando.", false);
+
+                for (String character : dividedCharacters.get(0)) {
+                    String title;
+                    if (character.matches("[0-9]")) {
+
+                        switch (character) {
+                            case "1":
+                                title = ":one:";
+                                break;
+                            case "2":
+                                title = ":two:";
+                                break;
+                            case "3":
+                                title = ":three:";
+                                break;
+                            case "4":
+                                title = ":four:";
+                                break;
+                            case "5":
+                                title = ":five:";
+                                break;
+                            case "6":
+                                title = ":six:";
+                                break;
+                            case "7":
+                                title = ":seven:";
+                                break;
+                            case "8":
+                                title = ":eight:";
+                                break;
+                            case "9":
+                                title = ":nine:";
+                                break;
+                            default:
+                                title = ":zero:";
+                                break;
+                        }
+
+                    } else {
+                        title = ":regional_indicator_" + character + ":";
+                    }
+                    builder.addField(title, String.join("\n", characters.get(character)), false);
+                }
+
+                ActionRow row = ActionRow.of(
+                        Button.of(ButtonStyle.PRIMARY, "previousHelp_" + root, "Pág. anterior", Emoji.fromUnicode("U+2B05")),
+                        Button.of(ButtonStyle.SECONDARY, "number", "1/" + dividedCharacters.size()).withDisabled(true),
+                        Button.of(ButtonStyle.PRIMARY, "nextHelp_" + root, "Pág. siguiente", Emoji.fromUnicode("U+27A1"))
+                        );
+
+                event.replyEmbeds(builder.build()).addActionRows(row).queue(
+                        msg -> msg.deleteOriginal().queueAfter(10, TimeUnit.MINUTES)
+                );
             } else {
                 String commandPath = option.getAsString();
 
                 // /help minecraft project add
 
-                String fixedPath = root + "." + String.join(".subcommand.", commandPath.split(" "));
+                String fixedPath = root + "." + String.join(".subcommands.", commandPath.split(" "));
 
                 if (help.contains(fixedPath)) {
 
@@ -52,13 +124,13 @@ public class HelpCommand extends ListenerAdapter {
 
                     EmbedBuilder builder = new EmbedBuilder();
                     builder.setColor(Color.GREEN);
-                    builder.setTitle("/" + commandPath);
-                    builder.addField(":open_file_folder: Descripción:", commandSection.getString("usage"), false);
+                    builder.setTitle(commandSection.getString("usage"));
+                    builder.addField(":open_file_folder: Descripción:", commandSection.getString("description"), false);
                     builder.addField(":label: Tiene(n) permisos:", commandSection.getString("permission"), false);
                     if (commandSection.contains("aliases")) {
                         List<String> lines = new ArrayList<>();
                         for (String alias : commandSection.getStringList("aliases")) {
-                            lines.add("• `/" + alias + "`");
+                            lines.add("• `" + alias + "`");
                         }
                         builder.addField(":paperclip: Aliases:", String.join("\n", lines), false);
                     }
