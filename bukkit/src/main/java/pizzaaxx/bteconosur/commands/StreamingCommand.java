dc11dce2bef7ddd73b9a50eea1d401e781228fab
PIZZAAXX <pedro2massmann@gmail.com>
2022-09-20 19:39:16 -0300

    fixed StreamingCommand

--- bukkit/src/main/java/pizzaaxx/bteconosur/commands/StreamingCommand.java
+++ bukkit/src/main/java/pizzaaxx/bteconosur/commands/StreamingCommand.java
@@ -16,7 +16,6 @@ import java.net.URL;
 
 import static pizzaaxx.bteconosur.Config.gateway;
 import static pizzaaxx.bteconosur.ranks.Streamer.streamerPrefix;
-import static pizzaaxx.bteconosur.BteConoSur.broadcast;
 
 public class StreamingCommand implements CommandExecutor {
 
@@ -42,57 +41,57 @@ public class StreamingCommand implements CommandExecutor {
                             embed.setColor(new Color(255, 0, 0));
                             embed.setThumbnail("https://cdn.discordapp.com/attachments/807694452214333482/846362783959744562/youtube-logo-5-2.png");
 
-                            broadcast(">+----------------+[-< §aSTREAMING§f >-]+----------------+<");
-                            broadcast(" ");
+                            plugin.broadcast(">+----------------+[-< §aSTREAMING§f >-]+----------------+<");
+                            plugin.broadcast(" ");
 
-                            broadcast("¡§a" + s.getName() + "§f está en directo en §4YOUTUBE§f!");
-                            broadcast(BookUtil.TextBuilder.of(args[0])
+                            plugin.broadcast("¡§a" + s.getName() + "§f está en directo en §4YOUTUBE§f!");
+                            plugin.broadcast(BookUtil.TextBuilder.of(args[0])
                                     .color(ChatColor.AQUA)
                                     .style(ChatColor.UNDERLINE)
                                     .onHover(BookUtil.HoverAction.showText("Click para abrir."))
                                     .onClick(BookUtil.ClickAction.openUrl(args[0]))
                                     .build());
 
-                            broadcast(" ");
-                            broadcast(">+----------------+[-< ========= >-]+----------------+<");
+                            plugin.broadcast(" ");
+                            plugin.broadcast(">+----------------+[-< ========= >-]+----------------+<");
 
                         } else if (args[0].contains("twitch")) {
                             embed.addField("¡" + s.getName() + " está en directo en TWITCH!", args[0], false);
                             embed.setColor(new Color(147, 32, 223));
                             embed.setThumbnail("https://cdn.discordapp.com/attachments/807694452214333482/846365871696773140/tw.png");
 
-                            broadcast(">+----------------+[-< §aSTREAMING§f >-]+----------------+<");
-                            broadcast(" ");
+                            plugin.broadcast(">+----------------+[-< §aSTREAMING§f >-]+----------------+<");
+                            plugin.broadcast(" ");
 
-                            broadcast("¡§a" + s.getName() + "§f está en directo en §5TWITCH§f!");
-                            broadcast(BookUtil.TextBuilder.of(args[0])
+                            plugin.broadcast("¡§a" + s.getName() + "§f está en directo en §5TWITCH§f!");
+                            plugin.broadcast(BookUtil.TextBuilder.of(args[0])
                                     .color(ChatColor.AQUA)
                                     .style(ChatColor.UNDERLINE)
                                     .onHover(BookUtil.HoverAction.showText("Click para abrir."))
                                     .onClick(BookUtil.ClickAction.openUrl(args[0]))
                                     .build());
 
-                            broadcast(" ");
-                            broadcast(">+----------------+[-< ========= >-]+----------------+<");
+                            plugin.broadcast(" ");
+                            plugin.broadcast(">+----------------+[-< ========= >-]+----------------+<");
 
                         } else if (args[0].contains("facebook")) {
                             embed.addField("¡" + s.getName() + " está en directo en FACEBOOK!", args[0], false);
                             embed.setColor(new Color(0, 120, 255));
                             embed.setThumbnail("https://cdn.discordapp.com/attachments/807694452214333482/846363420869001266/fbg.png");
 
-                            broadcast(">+----------------+[-< §aSTREAMING§f >-]+----------------+<");
-                            broadcast(" ");
+                            plugin.broadcast(">+----------------+[-< §aSTREAMING§f >-]+----------------+<");
+                            plugin.broadcast(" ");
 
-                            broadcast("¡§a" + s.getName() + "§f está en directo en §9FACEBOOK§f!");
-                            broadcast(BookUtil.TextBuilder.of(args[0])
+                            plugin.broadcast("¡§a" + s.getName() + "§f está en directo en §9FACEBOOK§f!");
+                            plugin.broadcast(BookUtil.TextBuilder.of(args[0])
                                     .color(ChatColor.AQUA)
                                     .style(ChatColor.UNDERLINE)
                                     .onHover(BookUtil.HoverAction.showText("Click para abrir."))
                                     .onClick(BookUtil.ClickAction.openUrl(args[0]))
                                     .build());
 
-                            broadcast(" ");
-                            broadcast(">+----------------+[-< ========= >-]+----------------+<");
+                            plugin.broadcast(" ");
+                            plugin.broadcast(">+----------------+[-< ========= >-]+----------------+<");
 
                         }
                         gateway.sendMessageEmbeds(embed.build()).queue();
