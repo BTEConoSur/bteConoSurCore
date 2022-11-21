26c09452c4197df03e1b250f8fa1396c2418ea5d
PIZZAAXX <pedro2massmann@gmail.com>
2022-11-21 00:43:34 -0300

    started AssetsRegistry

--- bukkit/src/main/java/pizzaaxx/bteconosur/WorldEdit/Assets/AssetsRegistry.java
+++ bukkit/src/main/java/pizzaaxx/bteconosur/WorldEdit/Assets/AssetsRegistry.java
@@ -1,19 +1,43 @@
 package pizzaaxx.bteconosur.WorldEdit.Assets;
 
+import pizzaaxx.bteconosur.BTEConoSur;
 import pizzaaxx.bteconosur.Registry.Registry;
+import pizzaaxx.bteconosur.SQL.Columns.SQLColumnSet;
+import pizzaaxx.bteconosur.SQL.Conditions.SQLConditionSet;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class AssetsRegistry implements Registry<String, Asset> {
 
+    private final BTEConoSur plugin;
+
     private final Map<String, Asset> assetsCache = new HashMap<>();
     private final Map<String, Long> deletionCache = new HashMap<>();
 
-    private final Set<String> ids;
+    private final Map<String, String> idsAndNames = new HashMap<>();
 
+    public AssetsRegistry(BTEConoSur plugin) {
+        this.plugin = plugin;
+    }
 
+    public void init() throws SQLException {
+        ResultSet set = plugin.getSqlManager().select(
+                "schematics",
+                new SQLColumnSet(
+                        "id"
+                ),
+                new SQLConditionSet()
+        ).retrieve();
+
+        while (set.next()) {
+            idsAndNames.put(set.getString("id"), set.getString("names"));
+        }
+    }
 
     @Override
     public boolean isLoaded(String id) {
@@ -42,6 +66,10 @@ public class AssetsRegistry implements Registry<String, Asset> {
 
     @Override
     public Set<String> getIds() {
+        return null;
+    }
+
+    public Set<String> getNames() {
         return null;
     }
 
