f460c309ad871bca15a90855a25f7e1347739d41
PIZZAAXX <pedro2massmann@gmail.com>
2022-12-27 16:45:11 -0300

    changed JDA version

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
     compileOnly("com.sk89q.worldedit:worldedit-bukkit:6.1.1-SNAPSHOT")
     compileOnly("com.sk89q.worldguard:worldguard-legacy:6.2")
     implementation("org.apache.commons:commons-text:1.9")
-    implementation("net.dv8tion:JDA:5.0.0-alpha.22")
+    implementation("net.dv8tion:JDA:5.0.0-beta.2")
     implementation("com.github.BuildTheEarth:terraplusplus:v1.1.0")
     implementation("xyz.upperlevel.spigot.book:spigot-book-api:1.6")
     implementation("fr.minuskube:netherboard-bukkit:1.2.2")
